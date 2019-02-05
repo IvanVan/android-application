19c6a2ddc4b63f974199765a9251482b9b0d7014
Ivan Vankovich <karatevanua@gmail.com>
2019-02-05 18:00:33 +0300

    init commit

---
+++ app/src/main/java/mds2019/ivan_vankovich/MainActivity.kt
@@ -0,0 +1,12 @@
+package mds2019.ivan_vankovich
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
