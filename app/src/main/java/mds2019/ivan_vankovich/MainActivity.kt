a6707a07e5e4ddb7509f03c820d565c09d1df6a2
Ivan Vankovich <karatevanua@gmail.com>
2019-02-06 01:30:11 +0300

    feature: added fabric service

--- app/src/main/java/mds2019/ivan_vankovich/MainActivity.kt
+++ app/src/main/java/mds2019/ivan_vankovich/MainActivity.kt
@@ -2,11 +2,14 @@ package mds2019.ivan_vankovich
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
     }
 }
