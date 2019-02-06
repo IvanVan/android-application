3ff1049901b0a7a78d5eee60e365a00dc3fcca9f
Ivan Vankovich <karatevanua@gmail.com>
2019-02-06 03:47:34 +0300

    feature: added appcenter service

--- app/src/main/java/mds2019/ivan_vankovich/MainActivity.kt
+++ app/src/main/java/mds2019/ivan_vankovich/MainActivity.kt
@@ -4,12 +4,18 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity : AppCompatActivity() {
+
+    val APP_KEY : String = "ccce248b-8f79-4637-bf10-d5a5d51db628"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
+        AppCenter.start(application, APP_KEY, Analytics::class.java, Crashes::class.java)
     }
 }
