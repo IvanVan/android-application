33f04d0189e53d89701fce38a1c9e8aba82f2b49
Ivan Vankovich <karatevanua@gmail.com>
2019-02-06 05:49:45 +0300

    feature: added distribute

--- app/src/main/java/mds2019/ivan_vankovich/MainActivity.kt
+++ app/src/main/java/mds2019/ivan_vankovich/MainActivity.kt
@@ -5,17 +5,16 @@ import android.os.Bundle
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.distribute.Distribute;
 
 class MainActivity : AppCompatActivity() {
 
-    private val APP_KEY : String = "ccce248b-8f79-4637-bf10-d5a5d51db628"
+    private val APP_CENTER_KEY : String = "ccce248b-8f79-4637-bf10-d5a5d51db628"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
-        AppCenter.start(application, APP_KEY, Analytics::class.java, Crashes::class.java)
+        AppCenter.start(application, APP_CENTER_KEY, Distribute::class.java)
     }
 }
